3945efa6a4ef2a59319d6d469d2b8bdf6be4604d
abdullahalkawser <abdullahalkawser1@gmail.com>
2024-10-21 06:28:13 +0600

    order status

--- src/app/(admin)/orders/[id].tsx
+++ src/app/(admin)/orders/[id].tsx
@@ -1,9 +1,11 @@
 import OrderListItem from '@/components/order/order'
 import OrderItemListItem from '@/components/order/OrderItemListItem'
+import Colors from '@/constants/Colors'
+import { OrderStatusList } from '@/types'
 import orders from 'assets/data/orders'
 import { Stack, useLocalSearchParams } from 'expo-router'
 import React from 'react'
-import { FlatList } from 'react-native'
+import { FlatList, Pressable } from 'react-native'
 
 import { View,Text } from 'react-native'
 
@@ -22,6 +24,41 @@ const OrderDetails = () => {
         data={order.order_items}
         renderItem={({ item }) => <OrderItemListItem item={item} />}
         contentContainerStyle={{ gap: 10 }}
+        ListFooterComponent={()=>(
+          <>
+
+  <Text style={{ fontWeight: 'bold' }}>Status</Text>
+  <View style={{ flexDirection: 'row', gap: 5 }}>
+    {OrderStatusList.map((status) => (
+      <Pressable
+        key={status}
+        onPress={() => console.warn('Update status')}
+        style={{
+          borderColor: Colors.light.tint,
+          borderWidth: 1,
+          padding: 10,
+          borderRadius: 5,
+          marginVertical: 10,
+          backgroundColor:
+            order.status === status
+              ? Colors.light.tint
+              : 'transparent',
+        }}
+      >
+        <Text
+          style={{
+            color:
+              order.status === status ? 'white' : Colors.light.tint,
+          }}
+        >
+          {status}
+        </Text>
+      </Pressable>
+    ))}
+  </View>
+</>
+
+        )}
       />
     </View>
   )
